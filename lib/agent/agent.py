d6824e1f06b4401ab40c67d3c3cc75a94db58164
Takato Yamazaki <takato.y7724@gmail.com>
2018-11-10 00:27:08 +0900

    fixed

--- lib/agent/agent.py
+++ lib/agent/agent.py
@@ -7,9 +7,5 @@ class Agent:
     def get_action(self, obs):
         raise NotImplementedError
 
-    def set_env(self, action_space, observation_space):
-        self.action_space = action_space
-        self.observation_space = observation_space
-
     def train(self, memory):
         pass
